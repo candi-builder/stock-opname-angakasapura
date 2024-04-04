807e3b3b061beddb5efef5f58f69712b99eeb648
azkifairuz <azkiajmal@gmail.com>
2024-04-05 00:45:52 +0700

    middleware

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\dashboard\Analytics;
 
 
 // Main Page Route
-Route::get('/', [Analytics::class, 'index'])->name('dashboard');
+
 
 
 Route::get('/user/login', [auth::class, 'formLogin'])->name("login");
@@ -21,7 +21,7 @@ Route::get('/user/forgot-password', [auth::class, 'forgotPW'])->name("forgot-pas
 
 Route::middleware('checkLogin')->group(function () {
   //user
-
+  Route::get('/', [Analytics::class, 'index'])->name('dashboard');
   Route::get('/user/list', [auth::class, 'index'])->name("get-list-user");
   Route::get('/user/tambah-user', [auth::class, 'formDaftarUser'])->name("add-new-user");
   Route::post('/user/register', [auth::class, 'register'])->name("register");
@@ -52,33 +52,32 @@ Route::middleware('checkLogin')->group(function () {
   Route::get('/report/stock/annual', [ReportController::class, 'showHistoriStockAnnual'])->name("stock-annual");
   Route::post('/filterData', [ReportController::class, 'filterData'])->name("filter-data-stock");
   Route::get('/stock/detail/today/{id}/{tanggal}/{jumlah}', [ReportController::class, 'detailReportToday'])->name("detail-stock-today");
-  
+
   // regions
-Route::get('/region/list', [RegionController::class, 'index'])->name("get-list-region"); 
-Route::post('/region/process-add', [RegionController::class, 'processAdd'])->name("process-add-region"); 
-Route::get('/region/edit/{id}', [RegionController::class, 'formEdit'])->name("edit-region");
-Route::post('/region/process-edit', [RegionController::class, 'processEdit'])->name("process-edit-region"); 
-Route::delete('/region/delete/{id}', [RegionController::class, 'processDelete'])->name("delete-region");
+  Route::get('/region/list', [RegionController::class, 'index'])->name("get-list-region");
+  Route::post('/region/process-add', [RegionController::class, 'processAdd'])->name("process-add-region");
+  Route::get('/region/edit/{id}', [RegionController::class, 'formEdit'])->name("edit-region");
+  Route::post('/region/process-edit', [RegionController::class, 'processEdit'])->name("process-edit-region");
+  Route::delete('/region/delete/{id}', [RegionController::class, 'processDelete'])->name("delete-region");
 
-// station
-Route::get('/station/list', [StationController::class, 'index'])->name("get-list-station");
-Route::post('station/process-add', [StationController::class,"processAdd"])->name("process-add-station");
-Route::get('/station/edit/{id}', [StationController::class, 'formEdit'])->name("edit-station");
-Route::post('/station/process-edit', [StationController::class, 'processEdit'])->name("process-edit-station"); 
-Route::delete('/station/delete/{id}', [StationController::class, 'processDelete'])->name("delete-station");
+  // station
+  Route::get('/station/list', [StationController::class, 'index'])->name("get-list-station");
+  Route::post('station/process-add', [StationController::class, "processAdd"])->name("process-add-station");
+  Route::get('/station/edit/{id}', [StationController::class, 'formEdit'])->name("edit-station");
+  Route::post('/station/process-edit', [StationController::class, 'processEdit'])->name("process-edit-station");
+  Route::delete('/station/delete/{id}', [StationController::class, 'processDelete'])->name("delete-station");
 
-// uom
-Route::get('/uom/list', [UomController::class, 'index'])->name("get-list-uom");
-Route::post('/uom/process-add', [UomController::class, 'processAdd'])->name("process-add-uom");
-Route::get('/uom/edit/{id}', [UomController::class, 'formEdit'])->name("edit-uom");
-Route::post('/uom/process-edit', [UomController::class, 'processEdit'])->name("process-edit-uom");
-Route::delete('/uom/delete/{id}', [UomController::class, 'processDelete'])->name("delete-uom");
+  // uom
+  Route::get('/uom/list', [UomController::class, 'index'])->name("get-list-uom");
+  Route::post('/uom/process-add', [UomController::class, 'processAdd'])->name("process-add-uom");
+  Route::get('/uom/edit/{id}', [UomController::class, 'formEdit'])->name("edit-uom");
+  Route::post('/uom/process-edit', [UomController::class, 'processEdit'])->name("process-edit-uom");
+  Route::delete('/uom/delete/{id}', [UomController::class, 'processDelete'])->name("delete-uom");
 
-// material-goup
-Route::get('/material-group/list', [MaterialGroupController::class, 'index'])->name('get-list-material-group');
-Route::post('/material-group/process-add', [MaterialGroupController::class, 'processAdd'])->name('process-add-material-group');
-Route::get('/material-group/edit/{id}', [MaterialGroupController::class, 'formEdit'])->name('edit-material-group');
-Route::post('/material-group/process-edit', [MaterialGroupController::class, 'processEdit'])->name('process-edit-material-group');
-Route::delete('/material-group/delete/{id}', [MaterialGroupController::class, 'processDelete'])->name('delete-material-group');
+  // material-goup
+  Route::get('/material-group/list', [MaterialGroupController::class, 'index'])->name('get-list-material-group');
+  Route::post('/material-group/process-add', [MaterialGroupController::class, 'processAdd'])->name('process-add-material-group');
+  Route::get('/material-group/edit/{id}', [MaterialGroupController::class, 'formEdit'])->name('edit-material-group');
+  Route::post('/material-group/process-edit', [MaterialGroupController::class, 'processEdit'])->name('process-edit-material-group');
+  Route::delete('/material-group/delete/{id}', [MaterialGroupController::class, 'processDelete'])->name('delete-material-group');
 });
-
